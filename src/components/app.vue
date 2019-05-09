1c7a81e6ec3b18db1444b498be43fffb466b946f
Nate <nkback@gmail.com>
2019-05-09 09:43:11 -0600

    Pull in axios as a dependency

--- src/components/app.vue
+++ src/components/app.vue
@@ -4,11 +4,20 @@
     </div>
 </template>
 <script>
+    import axios from 'axios';
     export default {
         data () {
             return {
-                header: "Welcome to Car Buyer Pro"
+                header: "Welcome to Car Buyer Pro",
+                bkg: chrome.extension.getBackgroundPage(),
             }
+        },
+        mounted() {
+            let vm = this;
+            axios.get("https://my-json-server.typicode.com/typicode/demo/posts")
+                .then((data) => {
+                    vm.bkg.console.log(data);
+                });
         }
      }
 </script>

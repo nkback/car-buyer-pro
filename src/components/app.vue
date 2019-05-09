4dc1e43e58311eb9a4c5d255c1b007f0f67752de
Nate <nkback@gmail.com>
2019-05-08 22:03:58 -0600

    Setup vue ground-work

---
+++ src/components/app.vue
@@ -0,0 +1,14 @@
+<template>
+    <div id="app">
+        <h1>{{header}}</h1>
+    </div>
+</template>
+<script>
+    export default {
+        data () {
+            return {
+                header: "Welcome to Car Buyer Pro"
+            }
+        }
+     }
+</script>
